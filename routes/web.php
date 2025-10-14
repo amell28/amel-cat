7435ec883465e8e9be3414aabc84c330eafd3dfc
amell28 <siti24si@mahasiswa.pcr.ac.id>
2025-10-14 09:21:32 +0700

    menerapkan tahap awal, create dan read

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MataKuliahController;
-
+use App\Http\Controllers\PelangganController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
@@ -44,3 +46,7 @@ Route::get('/pegawai',[PegawaiController::class, 'index']);
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 
+Route::get('/dashboard', [DashboardController::class, 'index'])
+		->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
